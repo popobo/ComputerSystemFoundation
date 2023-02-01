d87c406cb3daad7511b032494a4b16f11494bff9
popobo <331361996@qq.com>
2023-02-01 23:21:48 +0800

    add sll

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -59,6 +59,7 @@ static inline def_EHelper(irro) {
     switch (s->isa.instr.r.func3)
     {
     EX(0b000, irro_as)
+    EX(0b001, sll)
     EX(0b010, slt)
     EX(0b100, xor)
     EX(0b101, irro_sr)
