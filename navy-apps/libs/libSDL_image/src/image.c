ac86dc40a9fb1b3d0dd62ca1350d0cca86519eaf
popobo <331361996@qq.com>
2023-02-23 10:56:27 +0800

    run bird

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,10 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <assert.h>
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -11,7 +15,22 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+    int32_t fd = open(filename, O_RDONLY);
+    int32_t filesize = lseek(fd, 0, SEEK_END);
+    assert(filesize != -1);
+
+    uint8_t* buffer = (uint8_t *)malloc(filesize);
+    lseek(fd, 0, SEEK_SET);
+    int32_t read_size = read(fd, buffer, filesize);
+    assert(read_size == filesize);
+
+    SDL_Surface *surface = STBIMG_LoadFromMemory(buffer, filesize);
+    assert(surface != NULL);
+
+    close(fd);
+    free(buffer);
+
+    return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
